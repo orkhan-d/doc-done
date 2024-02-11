b8fec4f404aa0d76566cd10b3498936f3dc065c3
Orkhan Dzhafarli <orhan.jafarliev@gmail.com>
2024-02-11 21:33:48 +0300

    Added shacdn-ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from "@/components/navbar/Navbar.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      Lorem ipsum!
+      <Navbar/>
     </>
   )
 }
---
+++ client/src/components/navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Button} from "@/components/ui/button.tsx";
+
+const Navbar = () => {
+    return (
+        <div>
+            <Button variant="outline">Button</Button>
+
+        </div>
+    );
+};
+
+export default Navbar;

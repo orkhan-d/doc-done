3113802a79274773b3506edda71887dcd6888c3b
Orkhan Dzhafarli <orhan.jafarliev@gmail.com>
2024-02-11 19:08:51 +0300

    Added reponses in auth

--- api/auth/routers.py
+++ api/auth/routers.py
@@ -5,12 +5,15 @@ from api.auth.schemas import LoginData, RegisterData
 router = APIRouter(prefix='/auth')
 
 @router.post('/login')
-def login(data: LoginData, response: Response):
+def login(data: LoginData):
     res = login_user(data)
-    response.set_cookie('refresh_token', res['refresh_token'])
 
-    return res
+    return Response(res)
 
 @router.post('/register')
 def register(data: RegisterData):
     add_user(data)
+
+    return Response({
+        'message': 'Succesfully registered!'
+    }, 201)

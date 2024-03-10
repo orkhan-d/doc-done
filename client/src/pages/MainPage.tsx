e6814fe59a3d21252e22ccd82f93c47439bdc551
Orkhan Dzhafarli <orhan.jafarliev@gmail.com>
2024-03-10 22:36:23 +0300

    moved to next.js

--- client/src/pages/MainPage.tsx
+++
@@ -1,9 +0,0 @@
-const LoginPage = () => {
-    return (
-        <div className={"flex flex-1 flex-col justify-center items-center w-full h-full gap-3"}>
-
-        </div>
-    );
-};
-
-export default LoginPage;
